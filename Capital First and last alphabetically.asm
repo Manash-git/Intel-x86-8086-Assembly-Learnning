bd73b2b60ee44041096331aefb95f2ff61266793
Manash-git <manashmondalcse@gmail.com>
2017-10-28 01:07:32 +0600

    Complete code

--- Capital First and last alphabetically.asm
+++ Capital First and last alphabetically.asm
@@ -3,23 +3,28 @@ org 100h
 .stack 100h
 .model small
 .data  
-
+ first db '['  
+ last db '@'
+ nocap_msg db 10,13,"No capital.$"
+ cap_msg db 10,13,"Capital exits. $"
+ 
+ 
 .code  
 
 main proc 
 	mov ax,@data
 	mov ds,ax
 	
-	printn "Type a line :: "
+	print "Type a line :: "
 	mov ah,1
 	int 21h 
 	
-	check:
+	while:
 		
 		
 		
 		cmp al,0dh
-		je exit 
+		je end_while 
 		
 		cmp al,'A'
 		jl end_if
@@ -31,16 +36,51 @@ main proc
 		
 		mov first,al
 		
-		 cmp al,last
-		 jng end_if
+		check_last: 
+		
+		 	cmp al,last
+		 	jng end_if
 		 
-		 mov last,al
+		 	mov last,al
 		 
 		 end_if:
 		 	
 		 	int 21h
-		 	jmp check
-	
+		 	jmp while
+	    
+	  end_while: 
+	  
+	    mov ah,9
+	    
+	    cmp first,'['
+	    jne cap
+	    
+	    lea dx,nocap_msg
+	    
+	    jmp display
+	    
+	    cap:
+	    	lea dx,cap_msg 
+	    	int 21h
+	    	
+	    	printn ""
+	    	print "First:: "
+	    	mov ah,2
+	    	mov dl,first
+	    	int 21h 
+	    	printn ""
+	    	
+	    	print "Last:: "
+	    	mov ah,2
+	    	mov dl,last
+	    	int 21h
+	    	
+	    	jmp exit
+	    	
+	    display:
+	    	int 21h
+	    
+	    
 exit:
 	mov ah,4ch
 	int 21h 
