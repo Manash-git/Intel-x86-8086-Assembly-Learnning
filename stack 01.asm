90aca328296f1e56d5fdad3f7a7224577239c754
Manash-git <manashmondalcse@gmail.com>
2017-11-29 00:02:03 +0600

    before it was empty

--- stack 01.asm
+++ stack 01.asm
@@ -10,6 +10,24 @@ main proc
 	mov ax,@data
 	mov ds,ax
 	
+	mov ax,1234
+	mov bx,5678
+	mov cx,9781
+	mov dx,1256
+	
+	push cx
+	push dx
+	
+	pop bx 
+	
+	add cx,dx 
+;	mov ax,cx
+	xchg ax,cx
+	push ax
+	
+	pop ax
+	
+	call outdec
 	
 	mov ah,4ch
 	int 21h 
