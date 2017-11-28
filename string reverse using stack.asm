c9418afba451abd75c1f6c2e59950077bbbe5cb6
Manash-git <manashmondalcse@gmail.com>
2017-11-29 00:20:25 +0600

    code wrt empty file

--- string reverse using stack.asm
+++ string reverse using stack.asm
@@ -10,6 +10,36 @@ main proc
 	mov ax,@data
 	mov ds,ax
 	
+	print "Enter the String :: "
+	
+	xor cx,cx
+	mov ah,1
+	
+	read:
+	
+	int 21h
+	cmp al,0dh
+	je print_msg
+	inc cx
+	push ax
+	jmp read
+	
+	
+	print_msg:
+	
+	printn ""
+	print "Reverse String is :: "
+	jmp display
+	
+	display:
+	
+	jcxz exit
+	mov ah,2
+	pop dx 
+	int 21h
+	loop display
+	
+	exit:
 	
 	mov ah,4ch
 	int 21h 
