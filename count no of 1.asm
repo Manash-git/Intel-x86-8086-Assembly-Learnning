c36efc4a5201c1e1cf750f7eb92ca397404f51b2
Manash-git <manashmondalcse@gmail.com>
2017-12-13 01:10:30 +0600

    count using right shift

---
+++ count no of 1.asm
@@ -0,0 +1,20 @@
+include 'EMU8086.INC'
+org 100h
+.stack 100h
+.model small
+.data  
+
+.code  
+
+main proc 
+	mov ax,@data
+	mov ds,ax
+	
+	
+	mov ah,4ch
+	int 21h 
+	
+	main endp
+include outdec.asm
+include INDEC.ASM
+end main
